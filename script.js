19fea856012c7d1b0de421d0de5180403a695065
Bond Vlad <wlad23072003@gmail.com>
2024-04-17 10:45:12 +0300

    fix_alg

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const vstavkaSort = (arr) => {
   for (let i = 1, l = arr.length; i < l; i++) {
     const current = arr[i];
     let j = i;
-    while (j > 0 && arr[j - 1] > current) {
+    while (j > 0 && arr[j - 1] < current) {
       arr[j] = arr[j - 1];
       j--;
     }
